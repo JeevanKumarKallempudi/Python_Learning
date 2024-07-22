24a1268e65fd313532b0d1a64795c7cc3f738cf6
Jeevan K K <129277600+JeevanKumarKallempudi@users.noreply.github.com>
2024-07-22 17:35:52 +0530

    Update varibles.py

--- varibles.py
+++ varibles.py
@@ -1,4 +1,14 @@
 # Python Variables
 # Many values to Many variables 
 x, y, z = "K", "Jeevan", "Kumar"
+
+# One value to Many variable
+x = y = z = "Jeevan"
+
+#Unpack a collection
+Name = {'Kallempudi', 'Jeevan', "Kumar"}
+x, y, z = Name
+
 print(x)
+print(y)
+print(z)
